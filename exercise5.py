cdd3892696d30d5d22fa749f0cb0bba94f081160
Chinenye Ezeakor <chinenye.ezeakor@gmail.com>
2015-02-13 23:38:43 +0100

    2nd set

---
+++ exercise5.py
@@ -0,0 +1,15 @@
+my_name = 'Chinenye'
+my_age = 23 #errm true
+my_height = 70 #inches
+my_weight = 150 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." %my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth is %s." % my_teeth
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight) 
